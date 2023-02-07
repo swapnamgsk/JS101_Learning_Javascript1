5566d2d275066ee678b86c1513f3688f9e1e2bcd
swapnamgsk <mgswapna1911@gmail.com>
2023-02-07 10:47:36 +0530

    hello

---
+++ AshamedIcyLoopfusion (1)/problem2.js
@@ -0,0 +1,14 @@
+//Define a variable called name with your Name as the assigned value
+//Print the value stored in the variable name
+///Change the variable to store your father's name
+//Print the value stored in the variable name
+//Change the variable again to store your mother's name.
+//Print the value stored in the variable name
+
+
+let  Name = "swapna";
+console.log(Name);
+let fathersname = "govindaiah";
+console.log(fathersname);
+let mothersname= "ramadevi";
+console.log(mothersname);
---
+++ AshamedIcyLoopfusion (1)/problem4.js
@@ -0,0 +1,20 @@
+//Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+//Print the report card of the student (You can make it look nice by using some keyboard symbols )
+
+console.log("************************************")
+console.log( "Name    :swapna");
+console.log("School  :z.p.h.s.Dandapalli");
+console.log("Grade   : B");
+console.log("Roll.no : 22");
+console.log("************************************")
+console.log("Subject              Marks    ")
+console.log("________            ________    ")
+
+console.log("maths:               70    ")
+console.log("physics:             60    ")
+console.log("biology:             57    ")
+console.log("************************************");
+
+
+
+
